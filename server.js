52ac9170a510b22a82d808314119a6d7fbc8d59f
Nishant Sahoo <nishantsahoo.in@gmail.com>
2017-06-14 23:45:48 +0530

    Update

--- server.js
+++ server.js
@@ -2,20 +2,13 @@ const express = require('express');
 const bodyparser = require('body-parser');
 const app = express(); // express object
 
-// app.set("view engine", "hbs"); // not needed
-
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended: true}));
 
 app.use('/static', express.static(__dirname + "/public_static"));
 
-const CartApiRoute = require('./routes/cartapi');
-app.use('/api/cart', CartApiRoute);
-const ProductApiRoute = require('./routes/productapi');
-app.use('/api/product', ProductApiRoute);
-
-// const pageRoute = require('./routes/pages');
-// app.use('/todos', pageRoute);
+const MyApiRoute = require('./routes/myapi');
+app.use('/myapi/mycart', MyApiRoute);
 
 app.listen(2076, function () {
     console.log("Server started on http://localhost:2076");
